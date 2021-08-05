23d8cf002156d83a601bc56a19552b93cc522043
unknown <manoelprado.aecjr@gmail.com>
2021-08-05 12:08:51 -0300

    mousewheel: false - swiper

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const swiper = new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination'
   },
-  mousewheel: true,
+  mousewheel: false,
   keyboard: true,
   breakpoints: {
     767: {
